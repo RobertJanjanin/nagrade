3ab37b76e825f97877a4e9a0b2274cbb71a32f7b
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-05-04 11:59:31 +0200

    Add files via upload

---
+++ turnirAsistentModeli-nagrade/TurnirAsistentModel/TurnirAsistentModel/GlobalConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnirAsistentModel
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            
+            if (database)
+            {
+                //TODO-create a sql connection
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+
+            }
+            if (textFiles) {
+                //TODO - create the Text connection
+                TextConnection Text = new TextConnection();
+                Connections.Add(text);
+            }
+
+        }
+    }
+}
---
+++ turnirAsistentModeli-nagrade/TurnirAsistentModel/TurnirAsistentModel/Nagrada.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnirAsistentModel
+{
+    /// <summary>
+    /// Klasa za nagradu
+    /// </summary>
+    public class Nagrada
+    {
+        //The Unique identifier for the prize
+        public int Id { get; set; }
+        /// <summary>
+        /// Koje je osvojeno mjesto za ekipu.
+        /// </summary>
+        public int OsvojenoMjesto { get; set; }
+        /// <summary>
+        /// Lokacija gdje se odrzava utakmica.
+        /// </summary>
+        public string NazivMjesta { get; set; }
+        /// <summary>
+        /// Kolika je ukupna nagrada u turniru
+        /// </summary>
+        public decimal IznosNagrade { get; set; }
+        /// <summary>
+        /// Koliki ce biti postotak ukupne nagrade za prva 3 mjesta.
+        /// </summary>
+        public double PostotakNagrade { get; set; }
+    }
+}
